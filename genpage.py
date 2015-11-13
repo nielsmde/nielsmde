6c2e0c1d38c82b4118fc51219ae6e5cad2fde004
Niels Müller <post@nielsm.de>
2015-11-13 20:09:27 +0100

    Python-Script das den Markdown-Input in ein Template einbaut.

---
+++ genpage.py
@@ -0,0 +1,25 @@
+from subprocess import check_output
+
+def main():
+    content = {}
+
+    for c in 'contact', 'cv', 'work':
+        with open(c+'.md') as f:
+            content[c] = check_output(['pandoc', '-t', 'html'], stdin=f).decode(encoding='UTF-8')
+
+    with open('template.html') as f:
+        template = f.read()
+
+    template = template.replace('{','{{')
+    template = template.replace('}','}}')
+    template = template.replace('{{%','{')
+    template = template.replace('%}}','}')
+
+
+    with open('index.html', 'w') as out:
+        out.write(template.format(**content))
+
+    #call(['pandoc', '_index.html', '-o', '__index.html'])
+
+if __name__ == '__main__':
+    main()
